b1d76a3ddea544795de7b30321cace32708b2853
Amira Adediran <amira@Amiras-MacBook-Pro.local>
2020-05-06 16:40:56 +0100

    create actionCreators

--- state/actionCreators.js
+++ state/actionCreators.js
@@ -1,4 +1,17 @@
 import { GET_LOCATION, ADD_TO_FAVOURITE } from './actionTypes';
 
 
+function getLocation (){
+    
+    return {
+        type: GET_LOCATION,
+    }
+}
 
+
+function addToFav (item){
+    return {
+        type: ADD_TO_FAVOURITE,
+        payload: item
+    }
+}
