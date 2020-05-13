d3956b12c789624146084310a037134272d55adb
Amira Adediran <amira@Amiras-MBP.ipnxng.net>
2020-05-13 17:41:31 +0100

    add some styling

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import Signup from "./components/SignUp";
 import { Route, Redirect } from "react-router-dom";
 import LogIn from "./components/LogIn";
 import LocationList from "./components/LocationList";
-import CreateLocation from "./components/createLocation";
+import CreateLocation from "./components/CreateLocation";
+import Favourites from "./components/Favourites";
 
 
 function App() {
@@ -34,7 +35,17 @@ function App() {
           exact
           path="/createpost"
           render={props => PrivateRoute(CreateLocation, props)}
-        />
+      />
+      <Route
+        exact
+        path = "/favourite"
+        render = {props => {
+          return(
+            <Favourites {...props} locations = {locations} setLocations = {setLocations}/>
+          )
+        }}
+      />
+      
     </div>
   );
 }
--- src/components/createLocation.jsx
+++ src/components/createLocation.jsx
@@ -1,6 +1,73 @@
 import React, { useRef } from "react";
 import axiosWithAuth from "../axiosWithAuth/index";
+import styled from "styled-components";
 
+const NewPost = styled.div`
+    height: 100vh;
+    width: 100vw;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+
+    .newLocation{
+        width: 100%;
+        height: 100%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        background-color: #faedB8;
+    }
+
+    .newLocation-form{
+        border-radius: 20px;
+        width: 85%;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        height: 80%;
+        background-color: white;
+        padding: 0 2rem;
+    }
+
+    @media only screen and (min-width: 901px){
+        .newLocation-form{
+            width: 50%;
+        }
+    }
+
+
+    form{
+        // border: 1px solid green;
+        display: flex;
+        flex-direction: column;
+        height: 65%;
+    }
+
+    form input{
+        margin: 1rem 0;
+        padding: 0.8rem;
+    }
+
+    button{
+        width: 70%;
+        margin: auto;
+        padding: 1rem;
+        background-color: #faedB8;
+        border: 1px solid  #faedB8;   
+    }
+
+    button:hover{
+        background-color: #fff;
+        color: #c8c8c8;
+        font-weight: bold;
+    }
+
+    .post-location input{
+        margin: auto 0.5rem;
+    }
+`
 
 
 export default function CreateLocation(props){
@@ -15,7 +82,7 @@ export default function CreateLocation(props){
     function createlocation(event){
         event.preventDefault();
         axiosWithAuth()
-        .post('https://dreamlocations.herokuapp.com/api/location', {
+        .post('https://dreamlocations.herokuapp.com/api/location/', {
             description: description.current.value,
             imageURL: imageURL.current.value,
             city: city.current.value,
@@ -28,26 +95,30 @@ export default function CreateLocation(props){
             props.history.push("/home")
         })
         .catch(error =>{
-            alert('Please ensure all fields are filled in correctly')
+            alert(error)
         })
     }
 
 
     return(
-        <div className = "newLocation">
-            <form>
-                <h1>Create new Posting</h1>
-                <p>*please fill in all fields</p>
-                <input type="text" name = "image" placeholder = "image URL" ref={imageURL}/>
-                <input type="text" name = "description" placeholder = "description" ref={description}/>
-                <div className="post-location">
-                    <input type="text" name = "city" placeholder = "city" ref={city}/>
-                    <input type="text" name = "country" placeholder = "country" ref={country}/>
+        <NewPost>
+            <div className = "newLocation">
+                <div className = "newLocation-form">
+                    <h1>Create new Posting</h1>
+                    <p>*please fill in all fields</p>
+                    <form>
+                        <input type="text" name = "image" placeholder = "image URL" ref={imageURL}/>
+                        <input type="text" name = "description" placeholder = "description" ref={description}/>
+                        <div className="post-location">
+                            <input type="text" name = "city" placeholder = "city" ref={city}/>
+                            <input type="text" name = "country" placeholder = "country" ref={country}/>
+                        </div>
+                        <input type="number" name = "price" placeholder = "price" ref={price}/>
+                        <input type="number" name = "rating" placeholder = "rating" ref={rating}/>
+                        <button onClick = {createlocation}>Submit</button>
+                    </form>
                 </div>
-                <input type="text" name = "price" placeholder = "price" ref={price}/>
-                <input type="text" name = "rating" placeholder = "rating" ref={rating}/>
-                <button onClick = {createlocation}>Submit</button>
-            </form>
-        </div>
+            </div>
+        </NewPost>
     )
 }
